3dd60b4c883fd75f953c698dea27155ad60f1e28
unknown <mkbansford@gmail.com>
2020-08-04 15:25:28 +0000

    first commit

---
+++ HW2_id.js
@@ -0,0 +1,9 @@
+// const mainBody= document.getElementById("mainBody");
+// const button1= document.getElementById("btn1");
+// const myP1 = document.createElement("p");
+
+// mainBody.appendChild(myP1);
+
+// button1.addEventListener("click", function(){
+//     paragraph1.textContent=Date();
+// });
