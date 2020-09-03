8f8bf458fc40295e8275f0f1de8eb98d1873ff1b
unknown <mkbansford@gmail.com>
2020-09-03 10:56:54 +0000

    added my id

--- HW2_id.js
+++ HW2_id.js
@@ -1,9 +1,9 @@
-// const mainBody= document.getElementById("mainBody");
-// const button1= document.getElementById("btn1");
-// const myP1 = document.createElement("p");
+const mainBody= document.getElementById("mainBody");
+const button1= document.getElementById("btn1");
+const myP1 = document.createElement("p");
 
-// mainBody.appendChild(myP1);
+mainBody.appendChild(myP1);
 
-// button1.addEventListener("click", function(){
-//     paragraph1.textContent=Date();
-// });
+button1.addEventListener("click", function(){
+    myP1.textContent=Date();
+});
